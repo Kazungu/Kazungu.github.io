15122f6a5d754485bd6b580b0ec5f621f9e3ea28
kazungu61 <kazungu61@gmail.com>
2018-11-23 11:13:44 +0300

    intial commit

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Mobisave</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="device-mockups/device-mockups.min.css">
+    <link href="css/fontawesome-free/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+<body id="page-top">
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+  <div class="container">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">MOBISAVE</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      Menu
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav text-uppercase ml-auto">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#page-top">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#download">Dowload</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<header class="masthead">
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-lg-7 my-auto">
+            <div class="header-content mx-auto">
+              <h2 class="mb-4">This is a USSD/Shortcode service that enables SACCOs to register
+                  their members and collect savings via a mobile phone. Members
+                  can also access loans through our innovative platform.</h2>
+              <h2 class="mb-4">We are changing the way SACCOs relate to their members and
+encouraging a culture of savings in Kenya by making it convenient.</h2>
+              <button type="button" class="btn btn-primary btn-lg">Register</button>
+              <button type="button" class="btn btn-primary btn-lg">Login</button>
+            </div>
+          </div>
+          <div class="col-lg-5 my-auto">
+            <div class="device-container">
+              <div class="device-mockup galaxy_s3 portrait white">
+                <div class="device">
+                  <div class="screen">
+                    <!--<img src="img/slider/f1.jpg">-->
+                    <div id="screencontent" class="carousel slide" data-ride="carousel">
+                      <ul class="carousel-indicators">
+                        <li data-target="#screencontent" data-slide-to="0" class="active"></li>
+                        <li data-target="#screencontent" data-slide-to="1"></li>
+                        <li data-target="#screencontent" data-slide-to="2"></li>
+                      </ul>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="img/slider/f1.jpg" alt="image">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="img/slider/f2.jpg" alt="image">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="img/slider/f3.jpg" alt="image">
+                        </div>
+                      </div>
+                      <a class="carousel-control-prev" href="#screencontent" data-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                      </a>
+                      <a class="carousel-control-next" href="#screencontent" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                      </a>
+                    </div>
+                  <!--<div class="button">
+                  
+                  </div>-->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <section id="services">
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Services</h2>
+            <!--<h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>-->
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-danger"></i>
+              <i class="fas fa-users fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Member registration</h4>
+            <p class="text-muted">Mobisave helps SACCOs register members on the web platform and also using the USSD Code *632#</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-danger"></i>
+              <i class="fas fa-dollar-sign fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Revenue collection</h4>
+            <p class="text-muted">Members can make their contributions using mpesa and the transactions are viewed on the platform.</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-danger"></i>
+              <i class="fas fa-chart-bar fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Analytics</h4>
+            <p class="text-muted">Provides SACCOs with reports and analytics for informed decison making.</p>
+          </div>
+          <!--<div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-danger"></i>
+              <i class="fas fa-chart-bar fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">more services</h4>
+            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente,</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-danger"></i>
+              <i class="fas fa-chart-bar fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">more services</h4>
+            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente,</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-danger"></i>
+              <i class="fas fa-chart-bar fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">more services</h4>
+            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente,</p>
+          </div>-->
+        </div>
+    </section>
+    <section class="download bg-danger text-center" id="download">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <div class="section-heading">
+            <h3>DOWNLOAD MOBISAVE APP</h3>
+            <div class="badges">
+              <a class="badge-link" href="#"><img src="img/google-play-badge.svg" alt=""></a>
+              <a class="badge-link" href="#"><img src="img/app-store-badge.svg" alt=""></a>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    </section>
+    <!--<section id="pricing">
+
+    </section>-->
+    <section id="contact">
+    <div class="container-fluid bg-grey">
+  <h3 class="text-center">CONTACT</h3>
+  <div class="row">
+    <div class="col-md-5">
+      <span>
+        <p><i class="fa fa-map-marker" style="font-size:20px;color:red"></i> Bonfide house, Riverside Lane off Riverside Drive</p>
+        <p><i class="fa fa-phone" style="font-size:20px;color:red"></i> +254 700 111 222</p>
+        <p><i class="fa fa-envelope" style="font-size:20px;color:red"></i> info@mobisave.co.ke</p>
+      </span>
+    </div>
+    <div class="col-sm-7 slideanim">
+      <div class="row">
+        <div class="col-md-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-basic pull-right" type="submit">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+    <footer class="container-fluid text-center">
+      <p>&copy; Mobisave 2018. All Rights Reserverd</p>
+      <img src="img/mobisave.png" alt="mobisave"/>
+    </footer>
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
